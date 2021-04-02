39d049b93af638713693c427cc954e211070a087
Jeff Munowitch <jmunowitch@gmail.com>
2021-04-02 11:32:44 -0700

    feat: Add related resource to teams

--- src/resources/team.js
+++ src/resources/team.js
@@ -1,5 +1,19 @@
 import { BaseResource } from './index'
+import LeagueResource from './league'
+
+import { NumberNormalizer } from 'rest-resource/dist/helpers/normalization'
 
 export default class TeamResource extends BaseResource {
     static endpoint = '/teams'
+
+    static related = {
+        league: {
+            to: LeagueResource,
+            nested: true
+        }
+    }
+
+    static normalization = {
+        league: new NumberNormalizer(),
+    }
 }
