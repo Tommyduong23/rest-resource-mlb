84415d084bbaeb87f6f2020b3acfe3e1c0f006ff
Giulio Gratta <giulio@protools.io>
2021-04-22 13:14:56 -0700

    Modify test and validator from PR comments

--- tests/unit/spec/components/TeamLogo.spec.js
+++ tests/unit/spec/components/TeamLogo.spec.js
@@ -1,18 +1,22 @@
-import { shallowMount } from "@vue/test-utils"
-import { expect } from "chai"
+import { shallowMount } from '@vue/test-utils'
+import { expect } from 'chai'
 
-import TeamLogo from "@/components/TeamLogo"
+import TeamLogo from '@/components/TeamLogo'
 import TeamResource from '@/resources/team'
 
-let teamID = 0
-let testTeam = new TeamResource({
+const teamID = 0
+const testTeam = new TeamResource({
     id: teamID,
 })
 
-describe("TeamLogo", () => {
-    let props = {team: testTeam}
-    it("has a TeamLogo image when mounted", () => {
-        const wrapper = shallowMount(TeamLogo, {propsData: props})
-        expect(wrapper.html()).to.include('//www.mlbstatic.com/team-logos/'+teamID+'.svg')
+describe('components/TeamLogo.vue', () => {
+    it('Props(team): displays TeamLogo image when mounted', () => {
+        let props = {team: testTeam}
+        let wrapper = shallowMount(TeamLogo, {propsData: props})
+
+        let teamLogoComponentSrc = wrapper.find('v-img-stub').attributes('src')
+        let teamLogoSrcString = '//www.mlbstatic.com/team-logos/'+teamID+'.svg'
+
+        expect(teamLogoComponentSrc).to.equal(teamLogoSrcString)
     })
 })
