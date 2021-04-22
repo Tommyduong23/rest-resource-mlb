0a9d4e864136cbe47e4bc857c391f54144eb1620
Jeff Munowitch <jmunowitch@gmail.com>
2021-04-22 13:19:07 -0700

    chore: Update Base Spec names

--- tests/unit/spec/components/Base.spec.js
+++ tests/unit/spec/components/Base.spec.js
@@ -6,14 +6,14 @@ import { colors } from '@/config/teamColors'
 import Base from "@/components/Base"
 
 describe('components/Base.vue', () => {
-    it('Slot(Title): Default Title', () => {
+    it('Slot(Title): displays default title', () => {
         const wrapper = shallowMount(Base)
         const defaultTitle = 'Major League Baseball'
 
         expect(wrapper.find('h1').text()).to.include(defaultTitle)
     })
 
-    it('Slot(Title): Custom Title', () => {
+    it('Slot(Title): displays custom title', () => {
         const title = 'Test Title'
         const wrapper = shallowMount(Base, {
             slots: {
@@ -24,36 +24,36 @@ describe('components/Base.vue', () => {
         expect(wrapper.find('h1').text()).to.include(title)
     })
 
-    it('Slot(Default): Default Content', () => {
+    it('Slot(Default): displays default content', () => {
         const wrapper = shallowMount(Base)
         const defaultContent = 'Need to Implement'
 
         expect(wrapper.find('v-main-stub').text()).to.include(defaultContent)
     })
 
-    it('Slot(Default): Custom content', () => {
+    it('Slot(Default): displays custom content', () => {
         const content = 'Test Content'
         const wrapper = shallowMount(Base, {
             slots: {
-                'default': content 
+                'default': content
             }
         })
 
         expect(wrapper.find('v-main-stub').text()).to.include(content)
     })
 
-    it('Slot(top-right-menu): Custom Content', () => {
+    it('Slot(top-right-menu): displays custom content', () => {
         const content = 'Test Content'
         const wrapper = shallowMount(Base, {
             slots: {
-                'top-right-menu': content 
+                'top-right-menu': content
             }
         })
 
         expect(wrapper.find('v-app-bar-stub').text()).to.include(content)
     })
 
-    it('Props(colors): Default Colors', () => {
+    it('Props(colors): uses default colors', () => {
         const wrapper = shallowMount(Base)
 
         expect(wrapper.find('v-app-bar-stub').attributes('style')).to.equal('background-color: ' + colors.default.primary+';')
@@ -61,11 +61,11 @@ describe('components/Base.vue', () => {
         expect(wrapper.find('h1').attributes('style')).to.equal('color: ' + colors.default.secondary+';')
     })
 
-    it('Props(colors): Custom Colors', () => {
+    it('Props(colors): uses custom colors', () => {
         const testColors = {
             primary: 'blue',
             secondary: 'red'
-        } 
+        }
 
         const wrapper = shallowMount(Base, {
             propsData: {
